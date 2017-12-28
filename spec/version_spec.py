7df0f03f63e8a699347d2bda12d6d0f4bfcbfd18
Jose <jmgallego@alea-soluciones.com>
2017-12-28 13:25:39 +0100

    green test

--- spec/version_spec.py
+++ spec/version_spec.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # from doublex import Spy
-from expects import expect
+from expects import expect, equal
+import version_manager
 # from doublex_expects import have_been_called_with
 
 A_REPO = "Repo Name"
@@ -10,4 +11,4 @@ with describe('Check if all given libraries are updated'):
 
     with context('Given an empty library list'):
         with it('return name of the program and outdated library list as nothing'):
-            expect(my_sut.check(A_REPO,AN_EMPTY_REQUIREMENTS_FILE).equal("Repo Name has no outdated packages"))
+            expect(version_manager.check(A_REPO,AN_EMPTY_REQUIREMENTS_FILE)).to(equal("Repo Name has no outdated packages"))

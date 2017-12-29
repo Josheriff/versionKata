e4285c9bb12cd48a5dced5bca007b2ea738e55a0
Jose <jmgallego@alea-soluciones.com>
2017-12-29 12:19:38 +0100

    first spy red, but test must be completed

--- spec/version_spec.py
+++ spec/version_spec.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
-# from doublex import Spy
+from doublex import Spy
 from expects import expect, equal
 import version_manager
 # from doublex_expects import have_been_called_with
 
 A_REPO = "Repo Name"
 AN_EMPTY_REQUIREMENTS_FILE = {}
+A_REQUIREMENTS_FILE_WITH_SOME = "jquery"
 
 with describe('Check if all given libraries are updated'):
 
@@ -15,3 +16,12 @@ with describe('Check if all given libraries are updated'):
             result = version_manager.check(A_REPO,AN_EMPTY_REQUIREMENTS_FILE)
 
             expect(result).to(equal("Repo Name has no outdated packages"))
+
+    with context('Given a library list with some element'):
+        with it('check if the method that check the list is called'):
+
+            self.check_version = Spy()
+
+            version_manager.check(A_REPO,A_REQUIREMENTS_FILE_WITH_SOME)
+
+            expect(self.check_version).to(have_been_called_with('the library : '.format('here we have a variable conteining the libraries readed')).once)

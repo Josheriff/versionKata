af5f307832067e6711e931eac3e59cf92299e9f2
Jose <jmgallego@alea-soluciones.com>
2018-01-02 14:31:52 +0100

    the requirements project had a requirements problem....

--- spec/version_spec.py
+++ spec/version_spec.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 from doublex import Spy
 from expects import expect, equal
-import version_manager
-# from doublex_expects import have_been_called_with
+from doublex_expects import have_been_called
+
+from version_manager import VersionManager, Colaborador
 
 A_REPO = "Repo Name"
 AN_EMPTY_REQUIREMENTS_FILE = {}
@@ -13,15 +14,18 @@ with describe('Check if all given libraries are updated'):
     with context('Given an empty library list'):
         with it('return name of the program and outdated library list as nothing'):
 
+            version_manager = VersionManager()
             result = version_manager.check(A_REPO,AN_EMPTY_REQUIREMENTS_FILE)
 
             expect(result).to(equal("Repo Name has no outdated packages"))
 
     with context('Given a library list with some element'):
+        with before.each:
+            self.colaborador = Spy()
+            self.version_manager = VersionManager(self.colaborador)
+
         with it('check if the method that check the list is called'):
 
-            self.check_version = Spy()
+            self.version_manager.check(A_REPO, A_REQUIREMENTS_FILE_WITH_SOME)
 
-            version_manager.check(A_REPO,A_REQUIREMENTS_FILE_WITH_SOME)
-
-            expect(self.check_version).to(have_been_called_with('the library : '.format('here we have a variable conteining the libraries readed')).once)
+            expect(self.colaborador.algo_que_debe_llamarse).to(have_been_called)
--- version_manager.py
+++ version_manager.py
@@ -1,4 +1,20 @@
 # -*- coding: utf-8 -*-
 
-def check(repo,requirements):
-    return '{} has no outdated packages'.format(repo)
+class Colaborador(object):
+    def algo_que_debe_llamarse(self):
+        print 'soy un colaborador ou yeah'
+        return 'soy un colaborador'
+
+class VersionManager(object):
+
+    def __init__(self, colaborador=None):
+        self.colaborador = colaborador
+
+ #   def check_version(self):
+ #       return 'lala'
+
+
+    def check(self, repo, requirements):
+        if self.colaborador:
+            self.colaborador.algo_que_debe_llamarse()
+        return '{} has no outdated packages'.format(repo)
